ea7e2b77683f3a254fb7fbf7173dc9f09b4ce520
yaskov <yaskov@skbkontur.ru>
2023-09-26 20:02:33 +0500

    1 Практика Яськов А.М.

---
+++ SuperHeoes/SuperHeoes/Program.cs
@@ -0,0 +1,39 @@
+﻿var batman = SuperHero.CreateOrGetExisted(HeroName.Batman);
+var superman = SuperHero.CreateOrGetExisted(HeroName.Superman);
+
+var newBatman = SuperHero.CreateOrGetExisted(HeroName.Batman);
+var newSuperman = SuperHero.CreateOrGetExisted(HeroName.Superman);
+
+Console.WriteLine(batman == newBatman);
+Console.WriteLine(superman == newSuperman);
+Console.WriteLine(superman == batman);
+
+public class SuperHero
+{
+  private static SuperHero batman = null;
+  private static SuperHero superMan = null;
+
+  public static SuperHero CreateOrGetExisted(HeroName Name)
+  {
+    if (Name == HeroName.Batman)
+    {
+      batman ??= new SuperHero(Name.ToString());
+      return batman;
+    }
+
+    superMan ??= new SuperHero(Name.ToString());
+    return superMan;
+  }
+
+  private readonly string Name;
+  private SuperHero(string Name)
+  {
+    this.Name = Name;
+  }
+}
+
+public enum HeroName
+{
+  Batman,
+  Superman,
+}
