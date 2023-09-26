e708b5e4dbe7433db25bb173c3dc028cd95b7ce6
TIMERBOT1 <118817775+TIMERBOT1@users.noreply.github.com>
2023-09-26 22:19:07 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using ConsoleApp1;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+	static void Main(string[] args)
+	{
+		var a = new SuperHero("Superman");
+        Console.WriteLine(a.Name);
+        var b = new SuperHero("Batman");
+    }
+}
