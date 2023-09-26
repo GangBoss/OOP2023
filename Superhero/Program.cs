1a51dbef180b5e5b88efab528b6c2aa59dd55f3f
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-09-26 20:15:55 +0500

    Выполнил задание

--- Superhero/Program.cs
+++ Superhero/Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main()
         {
-            Console.WriteLine("1231");
+            var batman1 = Superhero.Create(Heroes.Batman);
+            var batman2 = Superhero.Create(Heroes.Batman);
+            var superman1 = Superhero.Create(Heroes.Superman);
+            var superman2 = Superhero.Create(Heroes.Superman);
+            Console.WriteLine(batman2 is null);
+            Console.WriteLine(superman2 is null);
         }
     }
 }
