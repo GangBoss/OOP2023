4ca30cd77c720b16c8b6dbe372db98f25625c77a
WoeromProg <nikita174trofimov@gmail.com>
2023-09-26 21:32:16 +0500

    v

---
+++ Trofimov_N_V/Program.cs
@@ -0,0 +1,26 @@
+﻿public class SuperHero
+{
+    private static SuperHero? _superMan;
+    private static SuperHero? _batman;
+    
+    public string Name { get; }
+
+    public SuperHero(string name)
+    {
+        Name = name;
+    }
+
+    public SuperHero GetOrCreateSuperMan()
+    {
+        if (_superMan == null)
+            _superMan = new SuperHero("SuperMan");
+        return _superMan;
+    }
+
+    public SuperHero GetOrCreateBatman()
+    {
+        if (_batman == null)
+            _batman = new SuperHero("Batman");
+        return _batman;
+    }
+}
