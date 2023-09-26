ca2fdbca3143c5f7e586c9e1dfea1336f747458d
Devilboi99 <turnickii.n@gmail.com>
2023-09-26 20:25:12 +0500

    solve

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿public class ServiceCollectionsSuperHero
+{
+    public class SuperHero
+    {
+        public string Name { get; }
+
+        private static SuperHero? _batman;
+        private static SuperHero? _superman;
+
+
+        private SuperHero(string name)
+        {
+            Name = name;
+        }
+
+        public static SuperHero GetSuperMan()
+        {
+            if (_superman == null)
+            {
+                _superman = new SuperHero("SuperMan");
+            }
+            
+            return _superman;
+        }
+
+        public static SuperHero GetBatman()
+        {
+            if (_batman == null)
+            {
+                _batman = new SuperHero("BatMan");
+            }
+
+            return _batman;
+        }
+    }
+}
