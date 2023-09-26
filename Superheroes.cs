ff5aa21093cc042ea685320845473af87e1f2dcf
Ramil Mavlutov <stirk2281337@gmail.com>
2023-09-26 20:12:24 +0500

    Активность 1

---
+++ Superheroes.cs
@@ -0,0 +1,30 @@
+﻿public class Superhero
+{
+    private static Superhero superman;
+    private static Superhero batman;
+
+    public string Name { get; private set; }
+
+    private Superhero(string name)
+    {
+        Name = name;
+    }
+
+    public static Superhero GetSuperman()
+    {
+        if (superman == null)
+        {
+            superman = new Superhero("Superman");
+        }
+        return superman;
+    }
+
+    public static Superhero GetBatman()
+    {
+        if (batman == null)
+        {
+            batman = new Superhero("Batman");
+        }
+        return batman;
+    }
+}
