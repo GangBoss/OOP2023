8b140ea548ce438d487aab35c1024d8d6c06fa2d
Pavel.Arzamasov <Pavel.Arzamasov@urfu.me>
2023-09-26 20:23:27 +0500

    asdasd

---
+++ Activity1/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Activity1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class Superhero
+    {
+        public string Name { get; }
+        private static Superhero batman;
+        private static Superhero superman;
+
+        private Superhero(string name)
+        {
+            Name = name;
+        }
+
+        public static Superhero CreateOrGetSuperhero(string name)
+        {
+            if (name == "Batman")
+            {
+                if (batman == null)
+                    batman = new Superhero("Batman");
+                return batman;
+            }
+            else if (name == "Superman")
+            {
+                if (superman == null)
+                    superman = new Superhero("Superman");
+                return superman;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
