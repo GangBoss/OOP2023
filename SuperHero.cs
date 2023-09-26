9f0257ab0e2ce597ec9907b5144de28e8152d8b9
TIMERBOT1 <118817775+TIMERBOT1@users.noreply.github.com>
2023-09-26 21:55:19 +0500

    Create SuperHero.cs

---
+++ SuperHero.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class SuperHero
+    {
+        private string name;
+        public string Name { get { return name; } set { this.NewHero(value);}}
+        static private bool BatmanIsHere;
+        static private bool SupermanIsHere;
+
+        public SuperHero(string name)
+        {
+            this.NewHero(name);
+        }
+
+        public void NewHero(string Name) { 
+            if (Name == "Batman" & !BatmanIsHere)
+            {
+                this.name = Name;
+                BatmanIsHere = true;
+            }
+            else if(Name == "Superman" & !SupermanIsHere){
+                this.name = Name;
+                SupermanIsHere = true;
+            }
+            else
+            {
+                throw new Exception("Вы ввели неверное имя героя или героев уже максимальное количество.");
+            }
+        }
+    }
+}
