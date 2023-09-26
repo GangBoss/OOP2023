b8a6a6e401dfab62698db75b6f80ec66d07d17c7
SomEnaMeforme <vv12345v54321vv@gmail.com>
2023-09-26 21:03:57 +0500

    Practice1

---
+++ SuperHero/SuperHero/SuperHero.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperHeroes
+{
+    public enum SuperHeroNames
+    {
+        Batman,
+        Superman
+    }
+    public class SuperHero
+    {
+        public SuperHeroNames Name { get; }
+        private static SuperHero? superman;
+        private static SuperHero? batman;
+
+        public static SuperHero CreateorGetSuperHero(SuperHeroNames name)
+        {
+            if (name == SuperHeroNames.Superman)
+            {
+                return superman ?? new SuperHero(SuperHeroNames.Superman);
+            }
+            return batman ?? new SuperHero(SuperHeroNames.Batman);
+        }
+
+        private SuperHero(SuperHeroNames name)
+        {
+            Name = name;
+            if (name == SuperHeroNames.Superman)
+            {
+                superman = this;
+            }
+            else
+            {
+                batman = this;
+            }
+        }
+
+    }
+}
