0c158ef7377991c983a9ca2cb70ec0ea6d4d5348
Alexey.Krokhalev <Alexey.Krokhalev@urfu.me>
2023-09-28 02:04:23 +0500

    Задание на практику

--- krohalev_1/Program.cs
+++ krohalev_1/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace krohalev_1
 {
+    
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            var b1 =  Superhero.CreateSuperhero( SuperheroType.batman);
+            var b2 = Superhero.CreateSuperhero(SuperheroType.batman);
+            var s1 = Superhero.CreateSuperhero(SuperheroType.superman);
+            var s2 = Superhero.CreateSuperhero(SuperheroType.superman);
+
+            Console.WriteLine("Второго бетмена не существет:   {0}", b2 is null);
+            Console.WriteLine("Второго супермена не существет: {0}", s2 is null);
+
+            Console.ReadKey();
+            
+
+
+
         }
     }
 }
---
+++ krohalev_1/Superhero.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace krohalev_1
+{
+
+
+    public enum SuperheroType
+    {
+        superman,
+        batman
+    }
+    public class Superhero
+    {
+        public readonly string Name;
+
+        private static Dictionary<SuperheroType, int> superheroes = new Dictionary<SuperheroType, int>();
+
+        private Superhero(string name )
+        {
+            this.Name=name;
+        }
+        public static Superhero CreateSuperhero(SuperheroType superheroType)
+        {
+            if (superheroes.ContainsKey(superheroType))
+            {
+                if (superheroes[superheroType] >= 1)
+                    return null;
+            }
+            else
+            {
+                superheroes.Add(superheroType, 1);
+            }
+
+            //superheroes[superheroType]++;
+            return new Superhero(superheroType.ToString());
+
+
+
+        }
+    }
+
+}
