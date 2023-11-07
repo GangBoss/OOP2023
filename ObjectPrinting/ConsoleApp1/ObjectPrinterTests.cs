68f46684d537fba1f3292087500ee169fae9b20c
SomEnaMeforme <98802409+SomEnaMeforme@users.noreply.github.com>
2023-11-07 21:27:34 +0500

    RushkovaOI5
    Не уверена насчёт номера практики, пусть будет 5

---
+++ ObjectPrinting/ConsoleApp1/ObjectPrinterTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+namespace ObjectPrinting.Tests
+{
+    [TestFixture]
+    public class ObjectPrinterAcceptanceTests
+    {
+        [Test]
+        public void Demo()
+        {
+            var person = new Person { Name = "Alex", Age = 19 };
+
+            var printer = ObjectPrinter.For<Person>();
+            //1. Исключить из сериализации свойства определенного типа
+            //2. Указать альтернативный способ сериализации для определенного типа
+            //3. Для числовых типов указать культуру
+            //4. Настроить сериализацию конкретного свойства
+            //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+            //6. Исключить из сериализации конкретного свойства
+
+            string s1 = printer.PrintToString(person);
+            Assert.AreEqual("System.Int32 Age - 19System.String Name - Alex",s1);
+            //1
+            var s2 = printer.ExcludeType(typeof(int)).PrintToString(person);
+            Assert.AreEqual("System.String Name - Alex", s2);
+            //2
+            Func<Person, string> serializer = (obj) => "try";
+            var s3 = printer.ExcludeType(typeof(int)).SetDifferentSerializer(serializer).PrintToString(person);
+            Assert.AreEqual("try", s3);
+
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            //8. ...с конфигурированием
+        }
+    }
+}
---
+++ ObjectPrinting/ConsoleApp1/PrintingConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace ObjectPrinting
+{
+    public class PrintingConfig<T>
+    {
+        private List<PropertyInfo> properties = new List<PropertyInfo>();
+        private Func<T, string> serializer;
+        public PrintingConfig()
+        {
+            properties.AddRange(typeof(T).GetProperties());
+            serializer = this.DefaultSerializer;
+
+        }
+        private string DefaultSerializer(T obj)
+        {
+            var sb = new StringBuilder();
+            foreach (PropertyInfo prop in properties)
+            {
+                sb.Append(String.Format("{1} {0} - {2}", prop.Name, prop.PropertyType, prop.GetValue(obj)));
+            }
+            return sb.ToString();
+        }
+        public string PrintToString(T obj)
+        {
+            return this.serializer(obj);
+        }
+        public  PrintingConfig<T> ExcludeType(Type type)
+        {
+            properties = properties.Where(prop => !prop.PropertyType.Equals(type)).ToList();
+            return this;
+        }
+        public PrintingConfig<T> SetDifferentSerializer(Func<T, string> serializer)
+        {
+            this.serializer = serializer; 
+            return this;
+        }
+    }
+}
