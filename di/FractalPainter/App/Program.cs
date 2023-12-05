82e2aa8b96aa42c781eccb92ace092e4a798efa5
Ramil Mavlutov <stirk2281337@gmail.com>
2023-12-05 20:20:15 +0500

    di container

---
+++ di/FractalPainter/App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using FractalPainting.App.Actions;
+using FractalPainting.Infrastructure.UiActions;
+using Ninject;
+using Ninject.Parameters;
+
+namespace FractalPainting.App
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            try
+            {
+                var container = new StandardKernel();
+
+                container.Bind<IUiAction>().To<SaveImageAction>();
+                container.Bind<IUiAction>().To<DragonFractalAction>();
+                container.Bind<IUiAction>().To<KochFractalAction>();
+                container.Bind<IUiAction>().To<ImageSettingsAction>();
+                container.Bind<IUiAction>().To<PaletteSettingsAction>();
+
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                Application.Run(container.Get<MainForm>());
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
